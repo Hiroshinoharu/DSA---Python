612c6ce490fdd3d664bc3417d216f564f5191b2a
Hiroshinoharu <maxceban2019@gmail.com>
2025-08-01 09:41:50 +0100

    Add linked list implementation with node structure and basic methods

---
+++ LinkedList.py
@@ -0,0 +1,26 @@
+from Node import Node
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert(self, value):
+        pass
+
+    def delete(self, value):
+        pass
+
+
+n1 = Node(3)
+n2 = Node(7)
+n3 = Node(2)
+n4 = Node(9)
+
+ll = LinkedList()
+ll.head = n1
+
+n1.next = n2
+
+n2.next = n3
+
+n3.next = n4
